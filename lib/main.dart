321ae4073e1c6930c8a81077ab99496350ff504f
Awaleed <a.waleed0011@gmail.com>
2020-05-02 08:20:42 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:audioplayers/audio_cache.dart';
 import 'package:flutter/material.dart';
-// import 'package:flutter/services.dart';
+import 'package:flutter/services.dart';
 
 void main() async {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await SystemChrome.setEnabledSystemUIOverlays([]);
-  // await SystemChrome.setPreferredOrientations(
-  //   [
-  //     DeviceOrientation.landscapeLeft,
-  //     DeviceOrientation.landscapeRight,
-  //   ],
-  // );
+  WidgetsFlutterBinding.ensureInitialized();
+  await SystemChrome.setEnabledSystemUIOverlays([]);
+  await SystemChrome.setPreferredOrientations(
+    [
+      DeviceOrientation.landscapeLeft,
+      DeviceOrientation.landscapeRight,
+    ],
+  );
   runApp(XylophoneApp());
 }
 
